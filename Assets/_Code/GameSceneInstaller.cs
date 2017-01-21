f7eea85189f6f6b5cb63b16e924702dcb162c1bb
Antonio Harger Rivero <antonio.harger@gmail.com>
2017-01-21 13:47:23 -0300

    Simple Tests

---
+++ Assets/_Code/GameSceneInstaller.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Zenject;
+
+[CreateAssetMenu()]
+public class GameSceneInstaller:ScriptableObjectInstaller {
+    public GameObject TilePrefab;
+    public GameObject BlockPrefab;
+    public Map map;
+    
+}
